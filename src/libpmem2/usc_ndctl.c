a4e556939d0e5d6e0fe778ec3ec012703a5c75bb
Oksana Salyk <oksana.salyk@intel.com>
2023-10-05 08:15:26 +0200

    pmem2: add USE_NDCTL env var to usc_ndctl file

--- src/libpmem2/usc_ndctl.c
+++ src/libpmem2/usc_ndctl.c
@@ -19,133 +19,148 @@
 #include "source.h"
 #include "region_namespace_ndctl.h"
 
+// Set USE_NDCTL environment variable to '1' to use functions with ndctl
+#define USE_NDCTL_VAR "USE_NDCTL"
+
 int
 pmem2_source_device_usc(const struct pmem2_source *src, uint64_t *usc)
 {
-	LOG(3, "type %d, uid %p", src->type, usc);
-	PMEM2_ERR_CLR();
+	char *env_config = os_getenv(USE_NDCTL_VAR);
+	if (env_config != NULL && env_config[0] == '1') {
+		LOG(3, "type %d, uid %p", src->type, usc);
+		PMEM2_ERR_CLR();
 
-	if (src->type == PMEM2_SOURCE_ANON) {
-		ERR("Anonymous source does not support unsafe shutdown count");
-		return PMEM2_E_NOSUPP;
-	}
+		if (src->type == PMEM2_SOURCE_ANON) {
+			ERR("Anonymous source does not support unsafe shutdown count");
+			return PMEM2_E_NOSUPP;
+		}
 
-	ASSERTeq(src->type, PMEM2_SOURCE_FD);
+		ASSERTeq(src->type, PMEM2_SOURCE_FD);
 
-	struct ndctl_ctx *ctx;
-	int ret = PMEM2_E_NOSUPP;
-	*usc = 0;
+		struct ndctl_ctx *ctx;
+		int ret = PMEM2_E_NOSUPP;
+		*usc = 0;
 
-	LOG(3, "ndctl_new()");
-	errno = ndctl_new(&ctx) * (-1);
-	if (errno) {
-		ERR("!ndctl_new");
-		return PMEM2_E_ERRNO;
-	}
+		LOG(3, "ndctl_new()");
+		errno = ndctl_new(&ctx) * (-1);
+		if (errno) {
+			ERR("!ndctl_new");
+			return PMEM2_E_ERRNO;
+		}
 
-	struct ndctl_region *region = NULL;
-	ret = pmem2_region_namespace(ctx, src, &region, NULL);
+		struct ndctl_region *region = NULL;
+		ret = pmem2_region_namespace(ctx, src, &region, NULL);
 
-	if (ret < 0)
-		goto err;
+		if (ret < 0)
+			goto err;
 
-	ret = PMEM2_E_NOSUPP;
+		ret = PMEM2_E_NOSUPP;
 
-	if (region == NULL) {
-		ERR(
-			"Unsafe shutdown count is not supported for this source");
-		goto err;
-	}
-
-	struct ndctl_dimm *dimm;
-
-	ndctl_dimm_foreach_in_region(region, dimm) {
-		long long dimm_usc = ndctl_dimm_get_dirty_shutdown(dimm);
-		if (dimm_usc < 0) {
-			ret = PMEM2_E_NOSUPP;
+		if (region == NULL) {
 			ERR(
 				"Unsafe shutdown count is not supported for this source");
 			goto err;
 		}
-		*usc += (unsigned long long)dimm_usc;
+
+		struct ndctl_dimm *dimm;
+
+		ndctl_dimm_foreach_in_region(region, dimm) {
+			long long dimm_usc = ndctl_dimm_get_dirty_shutdown(dimm);
+			if (dimm_usc < 0) {
+				ret = PMEM2_E_NOSUPP;
+				ERR(
+					"Unsafe shutdown count is not supported for this source");
+				goto err;
+			}
+			*usc += (unsigned long long)dimm_usc;
+		}
+
+		ret = 0;
+
+	err:
+		ndctl_unref(ctx);
+		return ret;
+	} else {
+		ERR("Cannot read device usc - ndctl is not available");
+		return PMEM2_E_NOSUPP;
 	}
-
-	ret = 0;
-
-err:
-	ndctl_unref(ctx);
-	return ret;
 }
 
 int
 pmem2_source_device_id(const struct pmem2_source *src, char *id, size_t *len)
 {
-	PMEM2_ERR_CLR();
+	char *env_config = os_getenv(USE_NDCTL_VAR);
+	if (env_config != NULL && env_config[0] == '1') {
+		PMEM2_ERR_CLR();
 
-	struct ndctl_ctx *ctx;
-	struct ndctl_dimm *dimm;
-	int ret;
-	struct ndctl_region *region = NULL;
-	const char *dimm_uid;
+		struct ndctl_ctx *ctx;
+		struct ndctl_dimm *dimm;
+		int ret;
+		struct ndctl_region *region = NULL;
+		const char *dimm_uid;
 
-	if (src->type == PMEM2_SOURCE_ANON) {
-		ERR("Anonymous source does not have device id");
-		return PMEM2_E_NOSUPP;
-	}
+		if (src->type == PMEM2_SOURCE_ANON) {
+			ERR("Anonymous source does not have device id");
+			return PMEM2_E_NOSUPP;
+		}
 
-	ASSERTeq(src->type, PMEM2_SOURCE_FD);
+		ASSERTeq(src->type, PMEM2_SOURCE_FD);
 
-	LOG(3, "ndctl_new()");
-	errno = ndctl_new(&ctx) * (-1);
-	if (errno) {
-		ERR("!ndctl_new");
-		return PMEM2_E_ERRNO;
-	}
+		LOG(3, "ndctl_new()");
+		errno = ndctl_new(&ctx) * (-1);
+		if (errno) {
+			ERR("!ndctl_new");
+			return PMEM2_E_ERRNO;
+		}
 
-	size_t len_base = 1; /* '\0' */
+		size_t len_base = 1; /* '\0' */
 
-	ret = pmem2_region_namespace(ctx, src, &region, NULL);
+		ret = pmem2_region_namespace(ctx, src, &region, NULL);
 
-	if (ret < 0)
-		goto err;
+		if (ret < 0)
+			goto err;
 
-	if (region == NULL) {
-		ret = PMEM2_E_NOSUPP;
-		goto err;
-	}
+		if (region == NULL) {
+			ret = PMEM2_E_NOSUPP;
+			goto err;
+		}
 
-	if (id == NULL) {
+		if (id == NULL) {
+			ndctl_dimm_foreach_in_region(region, dimm) {
+				dimm_uid = ndctl_dimm_get_unique_id(dimm);
+				if (dimm_uid == NULL) {
+					ret = PMEM2_E_NOSUPP;
+					goto err;
+				}
+				len_base += strlen(ndctl_dimm_get_unique_id(dimm));
+			}
+			goto end;
+		}
+
+		size_t count = 1;
 		ndctl_dimm_foreach_in_region(region, dimm) {
 			dimm_uid = ndctl_dimm_get_unique_id(dimm);
 			if (dimm_uid == NULL) {
 				ret = PMEM2_E_NOSUPP;
 				goto err;
 			}
-			len_base += strlen(ndctl_dimm_get_unique_id(dimm));
+			count += strlen(dimm_uid);
+			if (count > *len) {
+				ret = PMEM2_E_BUFFER_TOO_SMALL;
+				goto err;
+			}
+			strncat(id, dimm_uid, *len);
 		}
-		goto end;
-	}
 
-	size_t count = 1;
-	ndctl_dimm_foreach_in_region(region, dimm) {
-		dimm_uid = ndctl_dimm_get_unique_id(dimm);
-		if (dimm_uid == NULL) {
-			ret = PMEM2_E_NOSUPP;
-			goto err;
-		}
-		count += strlen(dimm_uid);
-		if (count > *len) {
-			ret = PMEM2_E_BUFFER_TOO_SMALL;
-			goto err;
-		}
-		strncat(id, dimm_uid, *len);
+	end:
+		ret = 0;
+		if (id == NULL)
+			*len = len_base;
+	err:
+		ndctl_unref(ctx);
+		return ret;
+	} else {
+		ERR("Cannot read device id - ndctl is not available");
+		return PMEM2_E_NOSUPP;
 	}
-
-end:
-	ret = 0;
-	if (id == NULL)
-		*len = len_base;
-err:
-	ndctl_unref(ctx);
-	return ret;
 }
