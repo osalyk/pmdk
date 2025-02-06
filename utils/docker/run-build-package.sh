bc34fc6cb10525ccfdc4499ec99a34b8a5edf53e
Oksana Salyk <oksana.salyk@hpe.com>
2025-02-06 12:40:39 +0100

    common: remove unneeded jobs

--- utils/docker/run-build-package.sh
+++ utils/docker/run-build-package.sh
@@ -17,15 +17,19 @@ git config user.email "test@package.com"
 git config user.name "test package"
 git tag -a 1.4.99 -m "1.4" HEAD~1 || true
 
+echo "CC1= "
 echo $CC
-echo $PMDK_CC
 
 echo "## Build package (and run basic tests)"
 pushd $WORKDIR
 export PCHECK_OPTS="-j2 BLACKLIST_FILE=${BLACKLIST_FILE}"
+echo "CC2= "
+echo $CC
 make -j$(nproc) $PACKAGE_MANAGER
 
 echo "## Build PMDK once more (clobber from packaging process cleared out some required files)"
+echo "CC3= "
+echo $CC
 make -j$(nproc)
 
 echo "## Test built packages"
